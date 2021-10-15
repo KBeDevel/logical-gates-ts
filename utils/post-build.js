5ec439a72ca2e7085fc38a1cfe9bd2030d12d6ab
Benjamin Calisto <theuserbit@protonmail.com>
2021-10-15 14:46:59 -0300

    fix: build workflow for rimraf step

--- utils/post-build.js
+++ utils/post-build.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const pkg = require('../package.json');
+const fs = require('fs')
+const pkg = require('../package.json')
 
-(async () => {
+(() => {
   fs.copyFileSync('LICENSE', './dist/LICENSE');
   fs.copyFileSync('README.md', './dist/README.md')
   fs.writeFileSync('./dist/package.json', JSON.stringify({ ...pkg }, null, 2))
