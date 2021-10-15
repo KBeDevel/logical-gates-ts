af5d7440b4d1f926b2a01da78322719da33b00c3
Benjamin Calisto <theuserbit@protonmail.com>
2021-10-15 14:56:21 -0300

    fix: build scripts implicit syntax error

--- utils/post-build.js
+++ utils/post-build.js
@@ -1,8 +1,8 @@
-const fs = require('fs')
-const pkg = require('../package.json')
+const fs = require('fs');
+const pkg = require('../package.json');
 
 (() => {
   fs.copyFileSync('LICENSE', './dist/LICENSE');
-  fs.copyFileSync('README.md', './dist/README.md')
-  fs.writeFileSync('./dist/package.json', JSON.stringify({ ...pkg }, null, 2))
+  fs.copyFileSync('README.md', './dist/README.md');
+  fs.writeFileSync('./dist/package.json', JSON.stringify({ ...pkg }, null, 2));
 })()
--- utils/pre-build.js
+++ utils/pre-build.js
@@ -1,15 +1,15 @@
-const fs = require('fs')
-const rimraf = require('rimraf')
-const path = require('path')
+const fs = require('fs');
+const rimraf = require('rimraf');
+const path = require('path');
 
 (async () => {
-  const distPath = path.resolve(__dirname, '..', 'dist')
+  const distPath = path.resolve(__dirname, '..', 'dist');
   if (fs.existsSync(distPath)) {
     await new Promise(resolve => {
       rimraf(distPath, () => {
-        resolve()
+        resolve();
       })
     })
   }
-  fs.mkdirSync(distPath)
+  fs.mkdirSync(distPath);
 })()
